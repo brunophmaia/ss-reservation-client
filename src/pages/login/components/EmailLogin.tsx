54c6715609cad0c24ef8df599ff8985d53a040fe
Bruno Maia <maiaphbruno@gmail.com>
2024-07-22 14:52:29 -0300

    change input to outline

--- src/pages/login/components/EmailLogin.tsx
+++ src/pages/login/components/EmailLogin.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDevice } from '@/context/device-context';
-import { Box, Button, FormControl, TextField, InputLabel, InputAdornment, IconButton, Input } from '@mui/material';
+import { Box, Button, FormControl, TextField, InputLabel, InputAdornment, IconButton, OutlinedInput  } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 import styles from './EmailLogin.module.scss';
 
@@ -49,21 +49,23 @@ export default function EmailLogin(){
                 inputProps={{
                   type: "email",
                 }}
-                variant="standard" />
+                variant="outlined" />
             </div>
             <div className='flex p-t-8'>
-              <FormControl className='w100 text-field-override' variant="standard">
-                <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-                <Input
+              <FormControl className='w100 text-field-override'>
+                <InputLabel htmlFor="outlined-adornment-password">{t('login.password')}</InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
                   required
-                  id="standard-adornment-password"
                   type={showPassword ? 'text' : 'password'}
+                  label={t('login.password')}
                   endAdornment={
                     <InputAdornment position="end">
                       <IconButton
                         aria-label="toggle password visibility"
                         onClick={handleClickShowPassword}
                         onMouseDown={handleMouseDownPassword}
+                        edge="end"
                       >
                         {showPassword ? <VisibilityOff /> : <Visibility />}
                       </IconButton>
