47880c54729754fef7b38bdea747ca98f7063088
Bruno Maia <maiaphbruno@gmail.com>
2024-07-22 13:46:29 -0300

    form email login and refactor

---
+++ src/pages/login/components/EmailLogin.tsx
@@ -0,0 +1,88 @@
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDevice } from '@/context/device-context';
+import { Box, Button, FormControl, TextField, InputLabel, InputAdornment, IconButton, Input } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import styles from './EmailLogin.module.scss';
+
+export default function EmailLogin(){
+
+    const router = useRouter();
+    const { isMobile } = useDevice();
+    const { t } = useTranslation();
+  
+    const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState(false);
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+
+    const [credentialError, setCredentialError] = useState(false);
+  
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => event.preventDefault();
+  
+    const handleEmailChange = (e: any) => {
+      setEmail(e.target.value);
+      setEmailError(!e.target.validity.valid);
+    };
+  
+    const handleSubmit = (e: any) => {
+      e.preventDefault();
+      setCredentialError(!e.target.checkValidity());
+    };
+  
+    return (
+      <div className={`${isMobile ? '' : styles.w50p}`}>
+        <div className={`${isMobile ? '' : styles.w70p}`}>
+          <div className='p-b-16'>{t('login.loginWithEmail')}</div>
+          <Box component="form" onSubmit={handleSubmit} noValidate>
+            <div className='flex'>
+              <TextField 
+                className='w100 text-field-override'
+                required
+                label={t('login.email')}
+                value={email}
+                onChange={handleEmailChange}
+                error={emailError}
+                helperText={emailError ? t('login.validEmail') : ""}
+                inputProps={{
+                  type: "email",
+                }}
+                variant="standard" />
+            </div>
+            <div className='flex p-t-8'>
+              <FormControl className='w100 text-field-override' variant="standard">
+                <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                <Input
+                  required
+                  id="standard-adornment-password"
+                  type={showPassword ? 'text' : 'password'}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+            </div>
+            <div className='flexRow p-t-32 p-b-16'>
+              <div className='flex'>
+                <Button type="submit" className='w100' variant="contained">{t('login.login')}</Button>
+              </div>
+              {credentialError && <div className={`p-t-4 ${styles.warning}`}>
+                {t('login.inputEmailPswd')}
+              </div>}
+            </div>
+          </Box>
+          <div className='p-t-32'><a className={styles.createAnc} onClick={() => router.push('/create-account')}>{t('login.createAccount')}</a></div>
+        </div>
+      </div>
+    );
+  }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -6,15 +6,7 @@ import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDevice } from '@/context/device-context';
 import styles from './login.module.scss';
-import Button from '@mui/material/Button';
-import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
+import EmailLogin from './components/EmailLogin';
 
 export default function Login() {
 
@@ -23,13 +15,6 @@ export default function Login() {
   const [ type, setType ] = useState<UserType>();
   const router = useRouter();
   const { query } = router;
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-  };
   
   useEffect(() => {
     if(router.isReady) {
@@ -57,46 +42,11 @@ export default function Login() {
     <div className='h100 w100 verticalCenter horizontalCenter'>
       <div className='w100'>
         <div className={`${isMobile ? 'p-l-16 p-r-16' : 'flex'}`}>
-
-          {/* BEGIN GOOGLE LOGIN*/}
           <div className={`verticalCenter horizontalCenter ${isMobile ? '' : `${styles.w50p}`}`}>
             <GoogleLogin onSuccess={responseMessage} onError={errorMessage as any} />
           </div>
-          {/* END GOOGLE LOGIN*/}
-          
           {isMobile && <div className='p-t-32 p-b-32 horizontalCenter'>{t('login.or')}</div>}
-
-          {/* BEGIN EMAIL LOGIN */}
-          <div className={`${isMobile ? '' : styles.w50p}`}>
-            <div className={`${isMobile ? '' : styles.w70p}`}>
-              <div className='p-b-16'>{t('login.loginWithEmail')}</div>
-              <div className='flex'><TextField className='w100 text-field-override' label={t('login.email')} variant="standard" /></div>
-              <div className='flex p-t-8'>
-                <FormControl className='w100 text-field-override' variant="standard">
-                  <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-                  <Input
-                    id="standard-adornment-password"
-                    type={showPassword ? 'text' : 'password'}
-                    endAdornment={
-                      <InputAdornment position="end">
-                        <IconButton
-                          aria-label="toggle password visibility"
-                          onClick={handleClickShowPassword}
-                          onMouseDown={handleMouseDownPassword}
-                        >
-                          {showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                      </InputAdornment>
-                    }
-                  />
-                </FormControl>
-              </div>
-              <div className='flex p-t-32 p-b-16'><Button className='w100' variant="contained">{t('login.login')}</Button></div>
-              <div className='p-t-32'><a className={styles.createAnc} onClick={() => router.push('/create-account')}>{t('login.createAccount')}</a></div>
-            </div>
-            
-          </div>
-          {/* END EMAIL LOGIN */}
+          <EmailLogin/>
         </div>
       </div>
     </div>
